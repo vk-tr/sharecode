56a097e7e79d06a169817af0d8f872b62934e6cb
vk-tr <vitkit99@gmail.com>
2023-04-19 21:38:06 +0400

    Initial project creation
    https://github.com/vk-tr/sharecode/issues/3

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <RouterView />
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+}
+
+</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+import * as Y from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+import { MonacoBinding } from 'y-monaco';
+
+import * as monaco from 'monaco-editor';
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+});
+
+const editorElement = ref(null);
+
+onMounted(() => {
+  const ydoc = new Y.Doc();
+
+  // Users using the same ID will share the same document
+  const provider = new WebrtcProvider(props.id, ydoc);
+  const ycontent = ydoc.getText('monaco');
+
+  const editor = monaco.editor.create(editorElement.value, {
+    value: '',
+    language: "javascript",
+    theme: 'vs-light',
+    fontSize: 13,
+    fontFamily:
+      'ui-monospace, Menlo, Monaco, "Cascadia Code", "Cascadia Mono", "Segoe UI Mono", "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro","Fira Mono", "Droid Sans Mono", "Courier New", monospace',
+    minimap: {
+      enabled: false,
+    },
+    scrollBeyondLastLine: false,
+    renderWhitespace: true
+  });
+
+  // Bind Yjs to the editor model
+  new MonacoBinding(ycontent, editor.getModel(), new Set([editor]), provider.awareness);
+});
+</script>
+
+<template>
+  <div class="editor" ref="editorElement"></div>
+</template>
+
+<style>
+.editor {
+  width: 100%;
+  height: 100%;
+}
+</style>
